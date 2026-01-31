db71c908fbad5ebb585d20f7c87042986c336a9e
Ubuntu <ubuntu@ip-172-31-34-36.ec2.internal>
2026-01-31 07:45:58 +0000

    Remove secrets, clean logs, and update welcome message

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,29 +1,14 @@
 package hello;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    // Gitleaks should catch this hardcoded secret below:
-    private String leakedSecret = "v8N1S+pL9xR5qT2uW4yZ7A1B3C5D7E9F1G3H5I7J"; 
-
-    @Value("${app.api.key:NOT_SET}")
-    private String apiKey;
-
-    @Value("${spring.datasource.password:NOT_SET}")
-    private String 86d855dbPassword;
-
     @GetMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! By Vignan";
-    }
-
-    @GetMapping("/config-check")
-    public String checkConfig() {
-        boolean isApiKeyLoaded = !apiKey.equals("NOT_SET");
-        return "API Key Loaded: " + isApiKeyLoaded;
+        return "Hello from Dinesh - DevSecOps Hackathon 🚀";
     }
 }
+
