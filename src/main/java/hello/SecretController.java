e910cf39c472594613bd3096aeabc5f40caaae98
S Chandini <skokila624@gmail.com>
2026-01-31 13:16:00 +0530

    Use environment variable for GitHub tokenSecret
    Replaced hardcoded GitHub token with environment variable.

--- src/main/java/hello/SecretController.java
+++ src/main/java/hello/SecretController.java
@@ -1,5 +1,6 @@
 @RestController
 public class SecretController {
     // Hardcoded API Token
-    private String githubToken = "ghp_nS1yOEcfP5pvfqJml36mF7AkyHsEU0IU36mF";
+    String githubToken = System.getenv("GITHUB_TOKEN");
+
 }
