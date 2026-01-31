f7a6dce6df5f5cf57eb726a2042ec8f0743abf75
vignan <vignan.kandela@gmail>
2026-01-31 06:14:05 +0530

    vulnerable dependencies added and owasp with failbuild block

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -4,19 +4,22 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser; // Add this
 import org.springframework.test.web.servlet.MockMvc;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc // This allows us to inject MockMvc
+@AutoConfigureMockMvc
 class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser // This simulates a logged-in user to bypass Security
     void testIndex() throws Exception {
         mvc.perform(get("/"))
            .andExpect(status().isOk())
